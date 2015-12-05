1f8b1fd58f61535d57cb362530fc9ca3d9d83823
romainjpf <romainjpf.forestier@gmail.com>
2015-12-05 16:27:36 +0100

    s

--- emulator-mips/emulator-mips/emulator.c
+++ emulator-mips/emulator-mips/emulator.c
@@ -1,1 +1,14 @@
-#include "emulator"
+#include "emulator.h"
+
+char* instructionToHex(char* instruction) {
+	char* hexInstruction = malloc(8 * sizeof(char));
+
+	char* operation = strtok(instruction, ' ');
+
+	if (strcmp(operation, "ADD") == 0) {
+
+	}
+
+
+	return hexInstruction;
+}
