c8a87a8f95478a29db33a59769a41dbacda653ea
zenoxs <amaury.civier@gmail.com>
2015-12-05 16:47:20 +0100

    a

--- emulator-mips/emulator-mips/emulator.c
+++ emulator-mips/emulator-mips/emulator.c
@@ -1,9 +1,14 @@
 #include "emulator.h"
 
+char* instructionToHex(char* instruction) {
+	char* hexInstruction = malloc(8 * sizeof(char));
+
+	char* operation = strtok(instruction, " ");
+
+	if (strcmp(operation, "ADD") == 0) {
+
+	}
 
 
-//Test
-
-
-
-//test
+	return hexInstruction;
+}
--- emulator-mips/emulator-mips/main.c
+++ emulator-mips/emulator-mips/main.c
@@ -19,6 +19,8 @@ int main(int argc, char * argv[]) {
 				//Lecture instruction
 				printf("emul-mips(interactif)>");
 				scanf_s("%s", &instruction, SIZE);
+                
+                instructionToHex(instruction);
 			}
 
 			break;
