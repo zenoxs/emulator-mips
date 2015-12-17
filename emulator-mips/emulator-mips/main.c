f87e42bcbc594019245423885e29f8a8cab39606
zenoxs <amaury.civier@gmail.com>
2015-12-17 16:32:51 +0100

    aaa
    aaa

--- emulator-mips/emulator-mips/main.c
+++ emulator-mips/emulator-mips/main.c
@@ -6,16 +6,17 @@ int main(int argc, char * argv[]) {
     
     char* instruction = malloc(SIZE * sizeof(char));
 	char** tableInstructions;
+	int nbIntructions = 0;
     
     switch (argc) {
-        case 1: //Mode interactif
+        case 2: //Mode interactif
             
             //Lecture instruction
             printf("emul-mips(interactif)>");
             fgets (instruction, SIZE, stdin);
             instruction = strbreak(&instruction, '\n');
             
-            while ((strcmp(instruction, "exit\n") != 0) && (strcmp(instruction, "EXIT\n") != 0)) {
+            while ((strcmp(instruction, "exit") != 0) && (strcmp(instruction, "EXIT") != 0)) {
                 //Affichage instruction hexadecimale
                 printf("%s\n", instructionToHex(instruction));
                 
@@ -28,18 +29,20 @@ int main(int argc, char * argv[]) {
             }
             
             break;
-        case 2: // Mode non-interactif
+        case 1: // Mode non-interactif
             
             //Lecture du programme
-			tableInstructions = readFile(argv[1]);
-            
+			/*tableInstructions = readInstructionFromFile("C:\Users\Romain\Source\Repos\emulator-mips\emulator-mips\emulator-mips\Debug\prog.txt", &nbIntructions);*/
             
             while (1) {
+				int i = 0;
+				printf("Instruction\t\t\tHexadecimal");
+				for (i = 0; i < nbIntructions; i++) {
                 //Affichage instruction hexadecimale
+					printf("%s\t\t\t%s", tableInstructions[i], instructionToHex(tableInstructions[i]));
                 
                 //Execution instruction
-                
-                //Lecture instruction
+				}
                 
             }
             break;
